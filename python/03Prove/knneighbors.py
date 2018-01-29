8b858e7fdadb72a08c95c919f86924049e4fb03f
Jacob Alldredge <jakekalldredge@gmail.com>
2018-01-29 14:04:45 -0700

    misc

--- python/03Prove/knneighbors.py
+++
@@ -1,8 +0,0 @@
-from model import KnnModel
-
-class KnnClassifier:
-    def __init__(self, k=1):
-        self.k = k
-
-    def fit(self, data_train, target_train):
-        return KnnModel(self.k,data_train,target_train)
--- python/03Prove/val.py
+++
@@ -1,8 +0,0 @@
-buying   = ['vhigh','high','med','low']
-maint    = ['vhigh','high','med','low']
-door     = ['2','3','4','5more']
-persons  = ['2','4','more']
-lug_boot = ['small','med','big']
-safety   = ['low','med','high']
-car_cols = [buying,maint,door,persons,lug_boot,safety]
-car_col_names = ['Buying','Maint','Doors','Persons','Lug_boot','Safety']
